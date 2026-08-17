168cdbaa62b1c94817617c24069a2422ea87c7d8
Harvir R <harvir.rajasanci@gmail.com>
2026-08-17 03:30:34 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::process;
 
+fn echo(arg: &str) {
+    let rest = arg
+        .split_once(char::is_whitespace)
+        .map(|(_, r)| r.trim())
+        .unwrap_or("");
+    println!("{}", rest);
+}
+
 fn main() {
+    let mut commands: HashMap<String, fn(&str)> = HashMap::new();
+    commands.insert(String::from("echo"), echo as fn(&str));
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-
-        let trimmed = input.trim();
-        let command = trimmed.split_whitespace().next().unwrap_or("");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let input = input.trim();
+        let command = input.split_whitespace().next().unwrap_or("");
 
         if command == "exit" {
             process::exit(0);
-        } else if command == "echo" {
-            let rest = trimmed
-                .split_once(char::is_whitespace)
-                .map(|(_, r)| r.trim())
-                .unwrap_or("");
-            println!("{}", rest);
+        }
+
+        if let Some(func) = commands.get(command) {
+            func(input);
         } else {
             println!("{}: command not found", command);
         }
